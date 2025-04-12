b1f53dee9497aba61d711f17d157e55e05ab36f7
Holacliente SAC <admin@holacliente.com>
2025-04-12 10:10:01 -0500

    Create UbigeoImportCommand.php

---
+++ src/Console/Commands/UbigeoImportCommand.php
@@ -0,0 +1,42 @@
+use Illuminate\Console\Command;
+
+<?php
+
+namespace TuNamespace\MiPlugin\Console\Commands;
+
+use Illuminate\Console\Command;
+use TuNamespace\MiPlugin\Models\Ubigeo; // Si tienes un modelo
+
+
+class UbigeoImportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ubigeo:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import Ubigeo data into the database';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Starting Ubigeo data import...');
+
+        // Add your logic to import Ubigeo data here
+        // Example: Reading a file and inserting data into the database
+
+        $this->info('Ubigeo data import completed successfully.');
+        return Command::SUCCESS;
+    }
+}
