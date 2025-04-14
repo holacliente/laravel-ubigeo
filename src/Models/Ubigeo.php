dc819c4e0af7ead5b4f934f73d1babb7aee8abbb
Holacliente SAC <admin@holacliente.com>
2025-04-13 22:37:43 -0500

    Update Ubigeo.php

--- src/Models/Ubigeo.php
+++ src/Models/Ubigeo.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Ubigeo\Departamento;
 use App\Models\Ubigeo\Provincia;
 use App\Models\Ubigeo\Distrito;
+use Exception;
 
 final class Ubigeo
 {
@@ -19,10 +20,32 @@ final class Ubigeo
         return Provincia::all();
     }
 
+    // public static function provincias(string $departamento)
+    // {
+    //     $result = Departamento::where('name', 'LIKE', "%{$departamento}%")->first();
+    //     if($result === null) {
+    //         throw new Exception("Departamento no encontrado");
+    //     }
+    //     return $result->provincias;
+    // }
+
     public static function distritos()
     {
         return Distrito::all();
     }
+
+    // public static function distritos(string $departamento, string $provincia)
+    // {
+    //     $result = Departamento::where('name', 'LIKE', "%{$departamento}%")->first();
+    //     if($result === null) {
+    //         throw new Exception("Departamento no encontrado");
+    //     }
+    //     $provincia = $result->provincias()->where('name', 'LIKE', "%{$provincia}%")->first();
+    //     if($provincia === null) {
+    //         throw new Exception("Distrito no encontrado");
+    //     }
+    //     return $provincia->distritos;
+    // }
 
     public function getDepartamento($codigo): string
     {
